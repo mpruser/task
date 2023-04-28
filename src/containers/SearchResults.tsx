94e97694a8a0db1f699214fe4ff0796a223a6f39
mpruser <mpruser.k@gmail.com>
2023-04-28 15:44:58 +0900

    feat: 검색내역에 Exception 적용

--- src/containers/SearchResults.tsx
+++ src/containers/SearchResults.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useSearchImage } from '@hooks';
-import { ImageCard, InfiniteSection, List } from '@components';
+import { ImageCard, InfiniteSection, List, Spinner, Exception, Compass } from '@components';
 
 /**
  * 검색 결과 container
  */
 export const SearchResults = styled(({ className }) => {
-  const { data, hasNextPage, isFetching, isInitialLoading, fetchNextPage } = useSearchImage();
+  const { data, hasNextPage, isFetching, isSuccess, isInitialLoading, fetchNextPage } = useSearchImage();
 
   if (isInitialLoading) {
-    return null;
+    return <Exception full visual={<Spinner size="large" />} message="" />;
   }
 
   if (!data.length) {
-    return null;
+    return <Exception full visual={<Compass />} message={isSuccess ? '일치하는 검색결과가 없습니다' : '이미지를 검색할 수 있습니다'} />;
   }
 
   return (
